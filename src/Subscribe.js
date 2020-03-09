ac7137b2a1399b522e77ee4209c76532fccd00e5
Neel Raja <neelrr1@gmail.com>
2020-03-09 00:23:54 -0400

    Fix subscribe page

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -6,11 +6,12 @@ import { Grid, Button } from '@material-ui/core';
 function Subscribe() {
     return (
         <div>
-            <Header text="Subscribe to our Mailing List!"/>
-            <br />
-            <Grid container direction="column" alignItems="center"> 
+            {/* <Header text="Subscribe to our Mailing List!"/> */}
+            {/* <br /> */}
+            <iframe scrolling="no" style={{ width: '100%', height: '850px' }} src="https://mailchi.mp/078739483982/email_list"></iframe>
+            {/* <Grid container direction="column" alignItems="center"> 
                 <Button variant="contained" color="primary" size="large">Join Now!</Button>
-            </Grid>
+            </Grid> */}
         </div>      
     );
 }
